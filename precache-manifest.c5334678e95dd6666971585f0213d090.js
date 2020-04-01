dc778528ebb5d45c1405b4c449c6c9d13fb7adb1
geloocli <geloocli@gmail.com>
2020-04-01 17:21:20 +0800

    Updates

---
+++ precache-manifest.c5334678e95dd6666971585f0213d090.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37ba88c1934e559bd1a82807dd1c8bb8",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "ba7448c81e6c1e5ad314",
+    "url": "/covid-tracker/static/css/main.1bddd959.chunk.css"
+  },
+  {
+    "revision": "9cb2a82d377c0d5aba85",
+    "url": "/covid-tracker/static/js/2.65c9e2a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/covid-tracker/static/js/2.65c9e2a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba7448c81e6c1e5ad314",
+    "url": "/covid-tracker/static/js/main.7687d30e.chunk.js"
+  },
+  {
+    "revision": "272ccb92299f1d5eab60",
+    "url": "/covid-tracker/static/js/runtime-main.1b9cc767.js"
+  }
+]);
